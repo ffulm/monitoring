ede07fd63a28726447aacc1f7626679676fcc5f5
Jan Hermann <jan@jan-hermann.de>
2018-03-04 16:06:15 +0100

    add dhcp-check

--- scripts/subscripts/dhcpcheck.sh
+++ scripts/subscripts/dhcpcheck.sh
@@ -4,4 +4,14 @@
 ##
 ## Version 1.0
 ##
+## require dhcping
 ## check dhcp
+
+s=10.33.64.1
+c=10.33.99.1
+h=00:11:22:33:44:55
+
+
+tempDHCP=$(/usr/sbin/dhcping -s $s -c $c -h $h)
+
+echo \'dhcpStatus\': \'$tempDHCP\' >> tmp.json
